fcc62f552723c36025fc739a5a6ad712a1ea7485
Vadym <turokvadim2510@gmail.com>
2022-05-25 14:47:55 +0300

    lazy loading

--- client/src/components/latestlist/LatestList.tsx
+++ client/src/components/latestlist/LatestList.tsx
@@ -1,32 +1,44 @@
-import React, {FC} from 'react';
-import './latest.scss'
-import {useAppSelector} from "../../hooks";
-import {formatTime} from "../../helpers";
-import {Link} from "react-router-dom";
+import React, { FC } from "react";
+import "./latest.scss";
+import { useAppSelector } from "../../hooks";
+import { formatTime } from "../../helpers";
+import { Link } from "react-router-dom";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 
 const LatestList: FC = () => {
-    const {todayPosts} = useAppSelector(state => state.posts)
+  const { todayPosts } = useAppSelector((state) => state.posts);
 
-    return (
-        <div className={'latestList'}>
-            <div className={'latestTitle'}>
-                <h3>Latest</h3>
-            </div>
-            <div className={'latestItems'}>
-                <ul>
-                    {todayPosts.length > 0 && todayPosts.map(post => (
-                            <li key={post.id}>
-                                <span>{formatTime(post.dateAndTimePublish)}</span>
-                                <Link to={`/posts/${post.id}`} className={'latestLink'}>
-                                    <h5>{post.title}</h5>
-                                </Link>
-                            </li>
-                    ))
-                    }
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className={"latestList"}>
+      <div className={"latestTitle"}>
+        <h3>Latest</h3>
+      </div>
+      <div className={"latestItems"}>
+        <ul>
+          {todayPosts.length > 0 &&
+            todayPosts.map((post) => (
+              <li key={post.id}>
+                <span>{formatTime(post.dateAndTimePublish)}</span>
+                <Link to={`/posts/${post.id}`} className={"latestLink"}>
+                  <h5>{post.title}</h5>
+                </Link>
+                <div className="latestPostReactions">
+                  <div className="latestPostReactionItem">
+                    <FavoriteBorderIcon className="latestPostReactionIconLike" />
+                    <span>{post.userLikes.length}</span>
+                  </div>
+                  <div className="latestPostReactionItem">
+                    <ChatBubbleOutlineIcon className="latestPostReactionIconComment" />
+                    <span>{post.comments.length}</span>
+                  </div>
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default LatestList;
--- client/src/components/postlist/PostList.tsx
+++ client/src/components/postlist/PostList.tsx
@@ -1,48 +1,65 @@
-import React, {FC} from 'react';
-import './postlist.scss'
+import React, { FC } from "react";
+import "./postlist.scss";
 import PostItem from "./postitem/PostItem";
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../../store";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../store";
 import Loader from "../loader/Loader";
-import {motion} from "framer-motion";
-import {fetchAllPostsByQuery} from "../../store/reducers/post/action-creators";
-
+import { motion } from "framer-motion";
+import { fetchAllPostsByQuery } from "../../store/reducers/post/action-creators";
 
 const PostList: FC = () => {
-    const {posts, status} = useSelector((state: RootState) => state.posts)
-    const {currentPage, totalPages} = useSelector((state: RootState) => state.posts.paginationInfo)
-    const dispatch = useDispatch()
+  const { posts, status } = useSelector((state: RootState) => state.posts);
+  const { currentPage, totalPages } = useSelector(
+    (state: RootState) => state.posts.paginationInfo
+  );
+  const dispatch = useDispatch();
 
-    const handleNext = () => {
-        if(currentPage !== totalPages){
-            dispatch(fetchAllPostsByQuery(currentPage + 1, 4))
-        }
+  const handleNext = () => {
+    if (currentPage !== totalPages) {
+      dispatch(fetchAllPostsByQuery(currentPage + 1, 4));
     }
-    const handlePrevious = () => {
-        if(currentPage !== 1){
-            dispatch(fetchAllPostsByQuery(currentPage - 1, 4))
-        }
+  };
+  const handlePrevious = () => {
+    if (currentPage !== 1) {
+      dispatch(fetchAllPostsByQuery(currentPage - 1, 4));
     }
+  };
 
-    return (
-        <div className={'postList'}>
-            {status !== 'succeeded'  ? <Loader/> : posts.length === 0 ? <div className={'noPosts'}>No posts yet</div> :
-                posts.map(post =>
-                    <motion.div key={post.id} initial={{opacity: 0}} animate={{opacity: 1}}>
-                        <PostItem displayImage={true} post={post}/>
-                    </motion.div>)
-            }
-            <div className={'pagination'}>
-                <button disabled={currentPage === 1} onClick={handlePrevious} className={'paginationBtn'}>⬅ Previous</button>
-                <div className={'paginationPages'}>
-                    {
-
-                    }
-                </div>
-                <button disabled={currentPage === totalPages} onClick={handleNext} className={'paginationBtn'}>Next ➡</button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={"postList"}>
+      {status !== "succeeded" ? (
+        <Loader />
+      ) : posts.length === 0 ? (
+        <div className={"noPosts"}>No posts yet</div>
+      ) : (
+        posts.map((post, index) => (
+          <motion.div
+            key={post.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <PostItem displayImage={index === 0 ? true : false} post={post} />
+          </motion.div>
+        ))
+      )}
+      <div className={"pagination"}>
+        <button
+          disabled={currentPage === 1}
+          onClick={handlePrevious}
+          className={"paginationBtn"}
+        >
+          ⬅ Previous
+        </button>
+        <button
+          disabled={currentPage === totalPages}
+          onClick={handleNext}
+          className={"paginationBtn"}
+        >
+          Next ➡
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PostList;
